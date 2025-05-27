d26dfd56ad039207c2327fbbb2a35cfe99e32528
Sitong <nickren93@gmail.com>
2025-05-27 14:02:12 -0400

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,20 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    #pass
+    nodes_to_check = [self.root]
+
+    while len(nodes_to_check) > 0:
+      node = nodes_to_check.pop(0)
+      if node['id'] == id:
+        return node
+      else:
+        nodes_to_check = node['children'] + nodes_to_check
+
+    return None
+  
+    
+
+        
+
+
